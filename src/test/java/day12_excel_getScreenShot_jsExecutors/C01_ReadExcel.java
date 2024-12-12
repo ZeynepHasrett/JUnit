cc974fa428fccc407e0dfc66d9ae5697e14a6a3f
ZeynepHasrett <158554030+ZeynepHasrett@users.noreply.github.com>
2024-12-12 16:41:59 +0300

    day12

--- src/test/java/day12_excel_getScreenShot_jsExecutors/C01_ReadExcel.java
+++ src/test/java/day12_excel_getScreenShot_jsExecutors/C01_ReadExcel.java
@@ -1,46 +1,107 @@
 package day12_excel_getScreenShot_jsExecutors;
 
+import dev.failsafe.internal.util.Assert;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class C01_ReadExcel {
 
     @Test
-    public void test01(){
+    public void test01() throws IOException {
 
         // Gerekli ayarlamalari yapip, ulkeler excelindeki Sayfa1’e gidin
-
-
-
-
+        String dosyaYolu = "src/test/java/day11_webTables_excelOtomasyon/ulkeler.xlsx";
+        FileInputStream fileInputStream = new FileInputStream(dosyaYolu);
+        Workbook workbook = WorkbookFactory.create(fileInputStream);
+        Sheet sayfa1Obj = workbook.getSheet("Sayfa1");
 
         // 1.satirdaki 2.hucreye gidin ve yazdirin
-
+        System.out.println(sayfa1Obj.getRow(0).getCell(1)); // Başkent (İngilizce)
 
         // 1.satirdaki 2.hucreyi bir string degiskene atayin
         //   ve degerinin “Başkent (İngilizce)” oldugunu test edin
+        String expectedHucreDegeri = "Başkent (İngilizce)";
+        String satir1Hucre2 = sayfa1Obj.getRow(0).getCell(1).getStringCellValue();
 
+        Assertions.assertEquals(expectedHucreDegeri, satir1Hucre2);
 
-        // 2.satir 4.cell’in afganistan’in baskenti “Kabil” oldugunu test edin
+        // 2.satir 4.cell’in Afganistan’in baskenti “Kabil” oldugunu test edin
+        String expectedData = "Kabil";
+        String actualData = sayfa1Obj.getRow(1).getCell(3).getStringCellValue();
 
+        Assertions.assertEquals(expectedData, actualData);
 
         // Ulke sayisinin 190 oldugunu test edin
+        int expectedUlkeSayisi = 190;
+        int actualUlkeSayisi = sayfa1Obj.getLastRowNum() + 1 - 1;
+        // +1  method bize index getiriyor, satir sayisini bulmak icin +1 ekliyoruz
+        // -1  basta baslik satiri oldugundan satir sayisindan 1 cikartarak ulke sayisini bulabiliriz
 
+        Assertions.assertEquals(expectedUlkeSayisi, actualUlkeSayisi);
 
         // Fiziki olarak kullanilan satir sayisinin 191 oldugunu test edin
+        int expectedKullanilanSatirSayisi = 191;
+        int actualKullanilanSatirSayisi = sayfa1Obj.getPhysicalNumberOfRows();
 
+        Assertions.assertEquals(expectedKullanilanSatirSayisi, actualKullanilanSatirSayisi);
 
-        // Ingilizce ismi Netherland olan ulkenin baskentinin turkce Amsterdam oldugunu test edin
+        // Ingilizce ismi Netherlands olan ulkenin baskentinin Turkce Amsterdam oldugunu test edin
+        for (int i = 1; i <= sayfa1Obj.getLastRowNum(); i++) {
+            String satirdakiUlkeIsmi = sayfa1Obj.getRow(i).getCell(0).getStringCellValue();
 
+            if (satirdakiUlkeIsmi.equals("Netherlands")) {
+                String actualTurkceBaskentIsmi = sayfa1Obj.getRow(i).getCell(3).getStringCellValue();
+                Assertions.assertEquals("Amsterdam", actualTurkceBaskentIsmi);
+                break;
+            }
+        }
 
         // Turkce baskent isimlerinde Ankara bulundugunu test edin
+        boolean ankaraVarmi = false;
 
+        for (int i = 1; i <= sayfa1Obj.getLastRowNum(); i++) {
+            String satirdakiTurkceBaskentIsmi = sayfa1Obj.getRow(i).getCell(3).getStringCellValue();
+
+            if (satirdakiTurkceBaskentIsmi.equals("Ankara")) {
+                ankaraVarmi = true;
+                break;
+            }
+
+        }
+
+        Assertions.assertTrue(ankaraVarmi);
 
         // Turkce baskent isminde buyuk A olan ulke sayisini bulun
+        int sayac = 0;
 
+        for (int i = 1; i <= sayfa1Obj.getLastRowNum() ; i++) {
 
-        // Ingilizce ismi B ile baslayan kac ulke bulundugunu test edin
+            if (sayfa1Obj.getRow(i).getCell(3).getStringCellValue().contains("A")){
+                sayac++;
+            }
+        }
 
+        System.out.println("Turkce baskent isminde buyuk A olan ulke sayisi : " + sayac);
 
+        // Ingilizce ismi B ile baslayan kac ulke oldugunu bulun
+        sayac = 0;
+
+        for (int i = 1; i <= sayfa1Obj.getLastRowNum(); i++) {
+
+            if (sayfa1Obj.getRow(i).getCell(0).getStringCellValue().startsWith("B")){
+                sayac++;
+            }
+
+        }
+
+        System.out.println("Ingilizce ismi B ile baslayan ulke sayisi : " + sayac);
 
     }
 
--- src/test/java/day12_excel_getScreenShot_jsExecutors/C03_WriteExcel.java
+++ src/test/java/day12_excel_getScreenShot_jsExecutors/C03_WriteExcel.java
@@ -1,34 +1,49 @@
 package day12_excel_getScreenShot_jsExecutors;
 
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class C03_WriteExcel {
 
     @Test
-    public void writeExcelTesti(){
+    public void writeExcelTesti() throws IOException {
 
         // 1) Adimlari takip ederek Sayfa1’e kadar gidelim
-
+        String dosyaYolu = "src/test/java/day11_webTables_excelOtomasyon/ulkeler.xlsx";
+        FileInputStream fileInputStream = new FileInputStream(dosyaYolu);
+        Workbook workbook = WorkbookFactory.create(fileInputStream);
+        Sheet sayfa1 = workbook.getSheet("Sayfa1");
 
         // 2) 1.satir 5.hucreye yeni bir cell olusturalim
-
+        sayfa1.getRow(0).createCell(4);
 
         // 3) Olusturdugumuz hucreye “Nufus” yazdiralim
-
+        sayfa1.getRow(0).getCell(4).setCellValue("Nufus");
 
         // 4) 2.satir nufus kolonuna 1500000 yazdiralim
-
+        sayfa1.getRow(1).createCell(4).setCellValue(1500000);
 
         // 5) 10.satir nufus kolonuna 250000 yazdiralim
-
+        sayfa1.getRow(9).createCell(4).setCellValue(250000);
 
         // 6) 15.satir nufus kolonuna 54000 yazdiralim
-
+        sayfa1.getRow(14).createCell(4).setCellValue(54000);
 
         // bos olan ilk satira
         // ingilizce ulke ismi JavaRepublic, ingilizce baskent olarak Selenium yazdiralim
+        int ilkBosSatirIndexi = sayfa1.getLastRowNum()+1;
 
-
+        sayfa1.createRow(ilkBosSatirIndexi);
+        sayfa1.getRow(ilkBosSatirIndexi).createCell(0).setCellValue("JavaRepublic");
+        sayfa1.getRow(ilkBosSatirIndexi).createCell(1).setCellValue("Selenium");
 
         // 7) Dosyayi kaydedelim
         /*
@@ -48,11 +63,13 @@ public class C03_WriteExcel {
             excel'e kaydetme islemi yapmadan once
             excel dosyasinin kapali oldugundan emin olmalisiniz
          */
-
+        fileInputStream.close();
+        FileOutputStream fileOutputStream = new FileOutputStream(dosyaYolu);
+        workbook.write(fileOutputStream);
 
         // 8)Dosyayi kapatalim
-
-
+        fileOutputStream.close();
+        workbook.close();
 
     }
 
